6b567afc5536c9972f1fee45df8df75734b4ad43
yssagomes <rayssabgomes10@gmail.com>
2023-05-18 08:31:44 -0300

    Criamos uma classe e fizemos os objetos Adão e Eva. Após criarmos os objetos, executamos comandos para eles falarem. Por fim, executamos o programa na tela.

---
+++ src/Pessoa.java
@@ -0,0 +1,12 @@
+public class Pessoa {
+    //Atributos de classe (definem a forma
+    String nome;
+    String sobrenome;
+
+    //Método da classe (comportamento
+    //Dentro de um método teremos um algoritmo
+    public void falar (){
+        System.out.println("falei");
+    }
+
+}
